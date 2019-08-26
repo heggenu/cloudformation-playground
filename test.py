2d95aeb615c34af99943fca0f1bdaa5573e66031
hegge.nu <hegge.nu@gmail.com>
2019-08-26 22:19:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -27,27 +27,31 @@ def lambda_handler(event, context):
     subprocess.run(["rm","-rf", "/tmp/cloudformation-playground"])
     subprocess.run(["rm","-rf", "/tmp/id_rsa"])
 
-    os.environ["GIT_SSH_COMMAND"] = "ssh -o UserKnownHostsFile=/tmp/known_hosts -i /tmp/id_rsa"
+    os.environ["GIT_SSH_COMMAND"] = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /tmp/id_rsa"
 
     newFile = open('/tmp/known_hosts','w+')
     newFile.write('github.com,140.82.118.3,140.82.118.4  ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==')
     newFile.close()
+    
+    ssm=boto3.client('ssm')
+    privateSSHKey=ssm.get_parameter(
+                  Name='/securestring/data',
+                  WithDecryption=True
+                )
 
     keyFile = open('/tmp/id_rsa','w+')
-    keyFile.write(f'''-----BEGIN OPENSSH PRIVATE KEY-----
-    ...
------END OPENSSH PRIVATE KEY-----
-    ''')
+    keyFile.write(privateSSHKey['Parameter']['Value'])
     keyFile.close()
 
-    subprocess.run(["chmod","400", "/tmp/id_rsa"])
 
+    subprocess.run(["chmod","700", "/tmp/id_rsa"])
 
-        
     # Clone the remote Git repository
     output=subprocess.check_output(
         ' && '.join([
             #'rm -rf /tmp/*',
+            'git config --global --add user.email "example@example.com"',
+            'git config --global --add user.name "example"',
             'cd /tmp',
             'git %s %s' % (git_command, remote_repository),
             'cd /tmp/cloudformation-playground',
@@ -55,12 +59,10 @@ def lambda_handler(event, context):
             'git init',
             'git %s %s' % (git_command_add_origin, new_remote_repository),
             'git add .',
-            'git commit -m "initial commit"',
-            'git push -u origin master'
+            'git commit -m initial',
+            'git push --force -u origin master'
         ]),
         stderr=subprocess.STDOUT,
         shell=True).decode()
     
-    print(output.split('\n'))
-
-lambda_handler('empty','empty')
+    #print(output.split('\n'))
